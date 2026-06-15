fc8db0d34659fb273235af8eb3831b05afbe6000
Mohammad TanvirAhmmed <tanvir262728@gmail.com>
2026-06-15 23:26:20 +0600

    query-5,6,7-done

--- QUERY.sql
+++ QUERY.sql
@@ -90,3 +90,37 @@ ON b.user_id = u.user_id
 INNER JOIN matches m
 ON b.match_id = m.match_id;
 
+-- Query 5: Display a comprehensive list of all users and their booking IDs, ensuring that fans who have never bought a ticket are still listed.
+
+SELECT
+    u.user_id,
+    u.full_name,
+    b.booking_id
+FROM users u
+LEFT JOIN bookings b
+    ON u.user_id = b.user_id
+ORDER BY u.user_id;
+
+-- Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings.
+
+SELECT
+    booking_id,
+    match_id,
+    total_cost
+FROM bookings
+WHERE total_cost >
+(
+    SELECT AVG(total_cost)
+    FROM bookings
+);
+
+-- Query 7: Retrieve the top 2 most expensive matches sorted by base ticket price, skipping the absolute highest premium match.
+
+SELECT
+    match_id,
+    fixture,
+    base_ticket_price
+FROM matches
+ORDER BY base_ticket_price DESC
+LIMIT 2
+OFFSET 1;
